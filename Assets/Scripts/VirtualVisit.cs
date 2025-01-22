a8dff6f9e80faf78dc0e3b7fae6cdc39b46763d9
daadan53 <166875105+daadan53@users.noreply.github.com>
2025-01-22 16:07:57 +0100

    Update VirtualVisit.cs

--- Assets/Scripts/VirtualVisit.cs
+++ Assets/Scripts/VirtualVisit.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class VirtualVisit : MonoBehaviour
